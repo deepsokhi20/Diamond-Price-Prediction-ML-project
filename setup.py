918129e3f6987997b7f9dc68cdd775a8722fa4cf
Navdeep Kaur Sokhi <deepsokhi2004@gmail.com>
2024-03-23 13:00:10 +0530

    first commit

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 from setuptools import find_packages,setup
 from typing import List
 
+HYPEN_E_DOT='-e .'
 
 def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
+        requirements=[req.replace("\n","") for req in requirements]
 
-        return requirements
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
 
 setup(
-    name='DiamondPricePrediction',
+    name='Diamond_predectionProject',
     version='0.0.1',
     author='Navdeep',
     author_email='deepsokhi2004@gmail.com',
